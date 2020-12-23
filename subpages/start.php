82fe432fef050f4a3b31b6dc812ab72a74c851db
ParamedicWolf <75755386+ParamedicWolf@users.noreply.github.com>
2020-12-23 23:25:40 +0100

    start page
    test 1

---
+++ subpages/start.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>START</title>
+
+    <link rel="stylesheet" href="./css/start.css">
+</head>
+<body>
+    <div class="grid">
+        <iframe src="https://discord.com/widget?id=634851154865422336&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+
+            <a class="weatherwidget-io" href="https://forecast7.com/de/52d5213d40/berlin/" data-label_1="BERLIN" data-label_2="Wetter" data-font="Open Sans" data-icons="Climacons Animated" data-theme="gray" >BERLIN Wetter</a>
+            <script>
+                !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+            </script>
+    </div>
+</body>
+</html>
